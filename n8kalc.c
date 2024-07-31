1923c255eebdd18a9472b850218aa3126d91c751
nkeoboupha <nkeoboupha@proton.me>
2024-07-31 12:57:33 -0400

    Added -h and --help

--- n8kalc.c
+++ n8kalc.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
+int main(int argc, char * argv[]){
+	if (argc > 1){
+		for (int i = 1; i < argc; i++){
+			if (strcmp(argv[i], "-h") == 0|| strcmp(argv[i], "--help") == 0){
+				printf("N8Kalc: Nate K's Calculator\n");
+				printf("Usage: <Operand1> <Operation Symbol> <Operand2>\n");
+				return 0;
+			}
+			printf("Invalid Argument Provided:\n");
+			printf("Try using -h or --help arguments\n");
+			return 1;
+		}
+	}
 	long double firstOperand, secondOperand;
 	char operationSymbol;
 	long double result;
+	printf("Calculator Active!\n");
+	printf(">>");
 	scanf("%Lf %c %Lf", &firstOperand, &operationSymbol, &secondOperand);
 	switch (operationSymbol){
 		case '+':
